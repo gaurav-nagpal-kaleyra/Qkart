656abc907cdf0c220552df3ed602aa011d6dee5a
CrioUser <crio-user@criodo.com>
2023-03-07 16:52:27 +0530

    “ME_QKART_FRONTEND_V2_MODULE_REGISTER”

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,7 +10,10 @@ import "./Register.css";
 
 const Register = () => {
   const { enqueueSnackbar } = useSnackbar();
-
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCpassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   // TODO: CRIO_TASK_MODULE_REGISTER - Implement the register function
   /**
@@ -35,7 +38,46 @@ const Register = () => {
    *      "message": "Username is already taken"
    * }
    */
-  const register = async (formData) => {
+  const register = async () => {
+    const data = {
+      username: username,
+      password: password,
+      confirmPassword: cpassword,
+    };
+    if (validateInput(data)) {
+      setLoading(true);
+      const url = config.endpoint + "/auth/register";
+      const body = {
+        username: username,
+        password: password,
+      };
+
+      axios
+        .post(url, body)
+        .then((res) => {
+          enqueueSnackbar("Registered Successfully", {
+            variant: "success",
+            autoHideDuration: 2000,
+          });
+          setLoading(false);
+        })
+        .catch((error) => {
+          if (error.response) {
+            if (error.response.status === 400) {
+              enqueueSnackbar(error.response.data.message, {
+                variant: "error",
+                autoHideDuration: 2000,
+              });
+            }
+          } else {
+            enqueueSnackbar(
+              "Something went wrong. Check that the backend is running, reachable and returns valid JSON.",
+              { variant: "error", autoHideDuration: 2000 }
+            );
+          }
+          setLoading(false);
+        });
+    }
   };
 
   // TODO: CRIO_TASK_MODULE_REGISTER - Implement user input validation logic
@@ -48,15 +90,59 @@ const Register = () => {
    * @returns {boolean}
    *    Whether validation has passed or not
    *
-   * Return false if any validation condition fails, otherwise return true.
-   * (NOTE: The error messages to be shown for each of these cases, are given with them)
-   * -    Check that username field is not an empty value - "Username is a required field"
-   * -    Check that username field is not less than 6 characters in length - "Username must be at least 6 characters"
-   * -    Check that password field is not an empty value - "Password is a required field"
-   * -    Check that password field is not less than 6 characters in length - "Password must be at least 6 characters"
-   * -    Check that confirmPassword field has the same value as password field - Passwords do not match
-   */
+   **/
   const validateInput = (data) => {
+    /**
+     * Validate the input values so that any bad or illegal values are not passed to the backend.
+     *
+     * @param {{ username: string, password: string, confirmPassword: string }} data
+     *  Object with values of username, password and confirm password user entered to register
+     *
+     * @returns {boolean}
+     *    Whether validation has passed or not
+     */
+    //  Return false if any validation condition fails, otherwise return true.
+    //  (NOTE: The error messages to be shown for each of these cases, are given with them)
+    //  Check that username field is not an empty value - "Username is a required field"
+
+    //  Check that username field is not less than 6 characters in length - "Username must be at least 6 characters"
+    //  Check that password field is not an empty value - "Password is a required field"
+    //  Check that password field is not less than 6 characters in length - "Password must be at least 6 characters"
+    //  Check that confirmPassword field has the same value as password field - Passwords do not match
+
+    if (data.username === "") {
+      enqueueSnackbar("Username is a required field", {
+        variant: "warning",
+        autoHideDuration: 2000,
+      });
+      return false;
+    } else if (data.username.length < 6) {
+      enqueueSnackbar("Username must be at least 6 characters", {
+        variant: "warning",
+        autoHideDuration: 2000,
+      });
+      return false;
+    } else if (data.password === "") {
+      enqueueSnackbar("Password is a required field", {
+        variant: "warning",
+        autoHideDuration: 2000,
+      });
+      return false;
+    } else if (data.password.length < 6) {
+      enqueueSnackbar("Password must be at least 6 characters", {
+        variant: "warning",
+        autoHideDuration: 2000,
+      });
+      return false;
+    } else if (data.password !== data.confirmPassword) {
+      enqueueSnackbar("Password do not match", {
+        variant: "warning",
+        autoHideDuration: 2000,
+      });
+      return false;
+    } else {
+      return true;
+    }
   };
 
   return (
@@ -78,6 +164,9 @@ const Register = () => {
             name="username"
             placeholder="Enter Username"
             fullWidth
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
           />
           <TextField
             id="password"
@@ -88,6 +177,9 @@ const Register = () => {
             helperText="Password must be atleast 6 characters length"
             fullWidth
             placeholder="Enter a password with minimum 6 characters"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
           <TextField
             id="confirmPassword"
@@ -96,15 +188,22 @@ const Register = () => {
             name="confirmPassword"
             type="password"
             fullWidth
+            onChange={(e) => {
+              setCpassword(e.target.value);
+            }}
           />
-           <Button className="button" variant="contained">
+          <Button className="button" variant="contained" onClick={register}>
             Register Now
-           </Button>
+          </Button>
+
+          <Box className="circular">
+            {loading ? <CircularProgress color="success" /> : " "}
+          </Box>
           <p className="secondary-action">
-            Already have an account?{" "}
-             <a className="link" href="#">
+            Already have an account?
+            <a className="link" href="#">
               Login here
-             </a>
+            </a>
           </p>
         </Stack>
       </Box>
